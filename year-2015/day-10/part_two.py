629ff5ecd784cf89ffad0e9f633503b2ced09dab
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2024-12-18 02:09:41 +0100

    Added day 10

---
+++ year-2015/day-10/part_two.py
@@ -0,0 +1,17 @@
+import re
+
+input_data = "1113222113"
+
+
+def look_and_say(_input: str) -> str:
+    result = ""
+    for match in re.finditer(r"(1+|2+|3+|4+|5+|6+|7+|8+|9+|0+)", _input):
+        result += str(len(match.group(0))) + match.group(0)[0]
+
+    return result
+
+
+for _ in range(50):
+    input_data = look_and_say(input_data)
+
+print(len(input_data))
