cd067d9724720a8b6085fd2f3a0b5c05998c1191
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2025-12-01 08:19:28 +0100

    Added day 1 of 2025

---
+++ year-2025/day-01/part_two.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+input_data = Path(__file__).parent.joinpath("input.txt").read_text()
+
+rotations = input_data.strip().splitlines()
+
+current_position = 50
+nr_of_times_pointing_to_zero = 0
+
+for rotation in rotations:
+    direction = rotation[0]
+    distance = int(rotation[1:])
+
+    for _ in range(distance):
+        current_position += 1 if direction == "R" else -1
+
+        if current_position < 0:
+            current_position += 100
+        elif current_position >= 100:
+            current_position -= 100
+
+        if current_position == 0:
+            nr_of_times_pointing_to_zero += 1
+
+print(nr_of_times_pointing_to_zero)
